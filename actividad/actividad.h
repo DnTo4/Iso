6f2a11b79ffdf97f0cfd7eb8b3e70789a7341a56
Andres <andres.morosjr@gmail.com>
2023-12-15 14:43:34 +0100

    update 3

--- actividad/actividad.h
+++ actividad/actividad.h
@@ -70,97 +70,10 @@ class Foro{
         Actividad GetActividad(std::string titulo);
         std::vector<std::string> GetTitulos();
         bool FileDatosActividades();
+        bool LeerDatosActividades();
+        void Control_actividad();
+        bool ExisteActividad(std::string title);
 
 };
-void Control_actividad(){
-    
-    Foro miForo;
 
-    int opcion;
-    std::string id_title;
-
-    // Agregar Actividad
-    std::string newTitle;
-    std::string newCreator;
-    std::string newDescription;
-    std::string search;
-
-    Actividad nuevaActividad(newTitle, newCreator);
-
-    do {
-        std::cout << "\nMenu Principal:\n";
-        std::cout << "1. Agregar Actividad\n";
-        std::cout << "2. Modificar Actividad\n";
-        std::cout << "3. Eliminar Actividad\n";
-        std::cout << "4. Mostrar Títulos de Actividades\n";
-        std::cout << "5. Guardar Datos en Archivo\n";
-        std::cout << "6. Mostrar Datos de Actividad\n";
-        std::cout << "0. Salir\n";
-        std::cout << "Seleccione una opción: ";
-        std::cin >> opcion;
-
-
-        switch (opcion) {
-            case 1:
-
-
-                std::cout << "Ingrese los componentes de la nueva actividad:\n";
-                std::cout << "Ingrese el titulo de la actividad:\n";
-                std::cin >> newTitle;
-                nuevaActividad.SetTitulo(newTitle);
-                std::cout << "Ingrese el nombre del creador de la actividad:\n";
-                std::cin >> newCreator;
-                nuevaActividad.SetCreador(newCreator);
-                std::cout << "Para introducir una descripción entre en el menu de modificación de la actividad";
-                std::cout<<'\n';
-                
-                
-                // Asegúrate de tener una función InputInfo() en tu clase Actividad
-                miForo.AddActividad(nuevaActividad);
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 2:
-                // Modificar Actividad
-                std::cout << "Ingrese el título de la actividad a modificar: ";
-                std::cin >> id_title;
-                miForo.ModActividad(miForo.GetActividad(id_title));
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 3:
-                // Eliminar Actividad
-                std::cout << "Ingrese el título de la actividad a eliminar: ";
-                std::cin >> id_title;
-                miForo.DeleteActividad(id_title);
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 4:
-                // Mostrar Títulos de Actividades
-                std::cout << "Títulos de Actividades:\n";
-                for (const auto &titulo : miForo.GetTitulos()) {
-                    std::cout << "- " << titulo << '\n';
-                }
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 5:
-                // Guardar Datos en Archivo
-                miForo.FileDatosActividades();
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 6:
-                std::cout<<"Introduzca el título de la Actividad de la que desea la informacion"<<std::endl;
-                std::cin>> search;
-                miForo.GetActividad(search).GetInfo();
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            case 0:
-                std::cout << "Saliendo del programa...\n";
-                std::cout<<"\n\n________________________________________________________\n\n\n";
-                break;
-            default:
-                std::cout << "Opción inválida. Intente de nuevo.\n";
-                break;
-        }
-
-    } while (opcion != 0);
-};
 #endif
--- actividad/foro.cc
+++ actividad/foro.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include "actividad.h"
 
 Actividad::Actividad(std::string titulo, std::string nombre_creador, std::string descripcion, int num_usuarios)
@@ -118,6 +119,54 @@ std::vector<std::string> Foro::GetTitulos() {
     return vecaux;
 };
 
+bool Foro::LeerDatosActividades(){
+    std::string nombreArchivo = "Lista_Actividades.txt";
+    std::ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        std::ofstream archivo_escritura(nombreArchivo);
+        // Verificar si la apertura para lectura fue exitosa después de intentar crear el archivo
+        if (!archivo_escritura.is_open()) {
+            std::cerr << "Error al abrir o crear el archivo: " << nombreArchivo << std::endl;
+            archivo_escritura.close();
+            return false;
+        }
+        else{
+            archivo_escritura.close();
+            return true;
+        }
+    }
+    
+    std::string linea;
+    while (std::getline(archivo, linea)) {
+        std::istringstream ss(linea);
+        Actividad actividad("empty","empty");
+
+        // Leer datos separados por '|'
+        std::string tempTitulo, tempCreador, tempDescripcion;
+        int tempNumUsers;
+
+        // Leer datos separados por '|'
+        std::getline(ss, tempTitulo, '|');
+        std::getline(ss, tempCreador, '|');
+        std::getline(ss, tempDescripcion, '|');
+        ss >> tempNumUsers;
+
+        // Utilizar los métodos de la clase para establecer los valores
+        actividad.SetTitulo(tempTitulo);
+        actividad.SetCreador(tempCreador);
+        actividad.SetDescripcion(tempDescripcion);
+        actividad.SetNumUsers(tempNumUsers);
+
+
+        // Agregar la actividad a la lista
+        list_actividades_.push_back(actividad);
+    }
+    archivo.close();
+    
+    std::cout << "Sincronizando datos..." << std::endl;
+    return true;
+};
+
 bool Foro::FileDatosActividades(){
     std::ofstream fichero("Lista_Actividades.txt", std::ios::app);
         if(fichero.is_open()){
@@ -153,4 +202,116 @@ std::string LongString(std::string descripcion)
 {
     std::getline(std::cin, descripcion);
     return descripcion;
+};
+bool Foro::ExisteActividad(std::string title){
+    for (auto it = list_actividades_.begin(); it != list_actividades_.end(); it++) {
+        if (title == it->GetTitulo()) {return true;}
+    }
+    return false;
+};
+
+void Foro::Control_actividad(){
+    
+    Foro miForo;
+
+    int opcion;
+    std::string id_title;
+
+    // Agregar Actividad
+    std::string newTitle;
+    std::string newCreator;
+    std::string newDescription;
+    std::string search;
+    
+    LeerDatosActividades();
+
+    Actividad nuevaActividad(newTitle, newCreator);
+
+    do {
+        std::cout<<"\n________________________________________________________";
+        std::cout << "\nMenu Principal:\n";
+        std::cout << "1. Agregar Actividad\n";
+        std::cout << "2. Modificar Actividad\n";
+        std::cout << "3. Eliminar Actividad\n";
+        std::cout << "4. Mostrar Títulos de Actividades\n";
+        std::cout << "5. Guardar Datos en Archivo\n";
+        std::cout << "6. Mostrar Datos de Actividad\n";
+        std::cout << "0. Salir\n";
+        std::cout << "Seleccione una opción: ";
+        std::cin >> opcion;
+        std::cout<<"\n________________________________________________________\n";
+
+
+        switch (opcion) {
+            case 1:
+                std::cout << "\nIngrese los componentes de la nueva actividad:\n";
+                std::cout << "Ingrese el titulo de la actividad:\n";
+                std::cin >> newTitle;
+                if(miForo.ExisteActividad(newTitle)){
+                    std::cout<<"Ese titulo ya esta en uso"<<std::endl;    
+                    break;
+                }
+                nuevaActividad.SetTitulo(newTitle);
+                std::cout << "Ingrese el nombre del creador de la actividad:\n";
+                std::cin >> newCreator;
+                nuevaActividad.SetCreador(newCreator);
+                std::cout << "Para introducir una descripción entre en el menu de modificación de la actividad";
+                std::cout<<'\n';
+                
+                // Asegúrate de tener una función InputInfo() en tu clase Actividad
+                miForo.AddActividad(nuevaActividad);
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            case 2:
+                // Modificar Actividad
+                std::cout << "\nIngrese el título de la actividad a modificar: ";
+                std::cin >> id_title;
+                miForo.ModActividad(miForo.GetActividad(id_title));
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            case 3:
+                // Eliminar Actividad
+                std::cout << "\nIngrese el título de la actividad a eliminar: ";
+                std::cin >> id_title;
+                miForo.DeleteActividad(id_title);
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            case 4:
+                // Mostrar Títulos de Actividades utilizando la lista list_actividades_
+                std::cout << "\nTítulos de Actividades:\n";
+                if(list_actividades_.empty()){
+                    std::cout<<"(La lista de actividades esta vacia)"<<std::endl;
+                }
+                for (auto it=list_actividades_.begin(); it!=list_actividades_.end();it++) {
+                    std::cout << "- " << it->GetTitulo() << '\n';
+                }
+                std::cout << "\n________________________________________________________\n";
+                break;
+            case 5:
+                // Guardar Datos en Archivo
+                miForo.FileDatosActividades();
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            case 6:
+                //Mostrar Info
+                std::cout<<"\nIntroduzca el título de la Actividad de la que desea la informacion"<<std::endl;
+                std::cin>> search;
+                if(miForo.ExisteActividad(search)==false){
+                    std::cout<<"\n***Ese titulo no existe***"<<std::endl;    
+                    break;
+                }
+                miForo.GetActividad(search).GetInfo();
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            case 0:
+                std::cout << "\nSaliendo del programa...\n";
+                miForo.FileDatosActividades();
+                std::cout<<"\n________________________________________________________\n";
+                break;
+            default:
+                std::cout << "\nOpción inválida. Intente de nuevo.\n";
+                break;
+        }
+
+    } while (opcion != 0);
 };
